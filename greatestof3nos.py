a1a71f87ee44e4969b4a5b0772d0a149c4173bfa
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-09-22 08:44:15 +0530

    Add files via upload

---
+++ greatestof3nos.py
@@ -0,0 +1,9 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+c=int(input("enter c value"))
+if a>b and a>c:
+	print(a,"is the greatest")
+elif b>c:
+	print(b,"is the greatest")
+else:	
+	print(c,"is the greatest")

c0aacb77794ec94f4fdc246968bc37a8a798cc70
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-09-29 09:05:47 +0530

    Add files via upload

---
+++ firstfunction.py
@@ -0,0 +1,35 @@
+def add():
+	a=10
+	b=20
+	c=a+b
+	print(c)
+add()	
+
+
+def sub():
+	a=10
+	b=10
+	c=a-b
+	print(c)
+sub()	
+
+
+def multiply():
+	a=1
+	b=2
+	c=a*b
+	return c
+y=multiply()	
+print(y)
+
+
+def divide():
+	a=30
+	b=3
+	c=a/b
+	return c
+x=divide()
+print(x)	
+
+
+

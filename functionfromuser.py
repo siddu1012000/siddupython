c3c1f4cbc062a62b0521a91b35880dbe0ab3c69f
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-09-29 09:08:59 +0530

    Add files via upload

---
+++ functionfromuser.py
@@ -0,0 +1,27 @@
+
+def add(a,b):
+	return a+b
+
+def mult(a,b):
+	return a*b
+
+def	sub(a,b):
+	return a-b
+
+def div(a,b):
+	return a/b
+
+i=int(input("enter a no"))
+j=int(input("enter another"))
+
+c=add(i,j)
+print("addition",c)
+
+d=mult(i,j)
+print("multiplcation",d)
+
+
+print("subtraction",sub(i,j))
+
+
+print("division",div(i,j))

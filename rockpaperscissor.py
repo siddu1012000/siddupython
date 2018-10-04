089c75a092cf4610ba04d89373ccb0a4b5ac2959
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-10-04 15:35:01 +0530

    Add files via upload

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,25 +1,36 @@
 import random
-rps={1:'rock',2:'paper',3:'scissor'}
-
-while True:
+rps={1:'r',2:'p',3:'s'}
+a=0
+b=0
+while a<3 and b<3:
+	
 	r=rps[random.randint(1,3)]
 	x=input("enter rock,paper or scissor")
 	print("u chose",x,"comp chose",r)
-	
 	if x==r:
 		print("draw")
-	elif x=='rock':
-		if r=='paper':
+	elif x=='r':
+		if r=='p':
 			print("u lose")
-		elif r=='scissor':	
-			print("u win")	
-	elif x=='paper':
-		if r=='scissor':
-			print("u lose")
-		elif r=='rock':
+			b=b+1 
+		else:	
 			print("u win")
-	elif x=='scissor':
-		if r=='rock':
+			a=a+1	
+	elif x=='p':
+		if r=='s':
 			print("u lose")
-		elif r=='paper':
+			b=b+1
+		else:
 			print("u win")
+			a=a+1
+	elif x=='s':
+		if r=='r':
+			print("u lose")
+			b=b+1
+		else:
+			print("u win")
+			a=a+1
+	if a==3:
+			print("u won the game")
+	elif b==3:
+			print("comp won the game")

09da3b744017912acc1455d4804de59075495263
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-10-11 15:49:20 +0530

    Add files via upload

---
+++ sendingmail.py
@@ -0,0 +1,8 @@
+import smtplib
+m=smtplib.SMTP('smtp.gmail.com', 587)
+m.starttls()
+m.login("siddu1012000@gmail.com","asdflkjh1029384756")
+content="whats up"
+m.sendmail("siddu1012000@gmail.com","hemanthkori007@gmail.com",content)
+print("Sent")
+m.quit()

d16976c280ec4a4d0390d9f9069aa3213d9225cb
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2018-11-10 10:26:58 +0530

    Add files via upload

---
+++ out.py
@@ -0,0 +1,20 @@
+pi@raspberrypi:~ $ python3 c.py
+Bhavna joshi
+cmru9261819
+Bhavna joshicmru9261819
+pi@raspberrypi:~ $ python3
+Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
+[GCC 6.3.0 20170124] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a="hello all"
+>>> a.lower()
+'hello all'
+>>> a.upper()
+'HELLO ALL'
+>>> a.replace("H","A")
+'hello all'
+>>> a.replace("h","a")
+'aello all'
+>>> b="  DL3"
+>>> b.strip()
+'DL3'

fe896d5706e9a84d9443ac0d7177c5511f9098fa
siddu1012000 <43031526+siddu1012000@users.noreply.github.com>
2020-10-10 11:02:11 +0530

    Update sendingmail.py

--- sendingmail.py
+++ sendingmail.py
@@ -1,7 +1,7 @@
 import smtplib
 m=smtplib.SMTP('smtp.gmail.com', 587)
 m.starttls()
-m.login("siddu1012000@gmail.com","asdflkjh1029384756")
+m.login("siddu1012000@gmail.com","1029384756")
 content="whats up"
 m.sendmail("siddu1012000@gmail.com","hemanthkori007@gmail.com",content)
 print("Sent")
